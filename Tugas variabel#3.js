0937929c419cf97a6f4066e0877bd1964a7faa92
Rendi220703 <128986959+Rendi220703@users.noreply.github.com>
2023-10-19 16:17:39 +0700

    Add files via upload

---
+++ Tugas variabel#3.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "Saung Loa",
+    city: "Cibatu",
+    favoriteDrink: "Kukubima Milk",
+    favoriteFood: "Seblak Ceker",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
---
+++ Tugas#3.js
@@ -0,0 +1,15 @@
+const restaurant = [
+    {
+      name: "Saung Loa",
+      city: "Cibatu",
+      favoriteDrink: "Kukubima Milk",
+      favoriteFood: "Seblak Ceker",
+      isVegan: true,
+    },
+  ];
+  
+  console.log("Nama : " + restaurant[0].name);
+  console.log("Kota : " + restaurant[0].city);
+  console.log("Minuman Favorit : " + restaurant[0].favoriteDrink);
+  console.log("Makanan Favorit : " + restaurant[0].favoriteFood);
+  console.log("Vegan : " + restaurant[0].isVegan);
